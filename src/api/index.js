83c1588546aea2ae052c42fc09331bf987771e85
Sudeep Sharma <sharma.sudip1122@gmail.com>
2023-12-13 22:48:40 +0545

    Added ejs as view renderer and done calculation of percentage for one repo.

--- src/api/index.js
+++ src/api/index.js
@@ -1,30 +1,14 @@
-import express from "express";
 import { Octokit } from "octokit";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const router = express.Router();
-
 const octokit = new Octokit({
   auth: process.env.GITHUB_TEMP_TOKEN
 });
 
-router.get("/", (req, res) => {
-  let username = req.query.username;
-  let repo = req.query.repo;
-  try {
-    getRepoLanguages(username, repo).then(langs => {
-      res.send(langs);
-      console.log(langs);
-    });
-  } catch (error) {
-    console.error('Error fetching repo languages:', error);
-    throw error;
-  }
-});
 
-async function getRepoLanguages(owner, repo) {
+export async function getRepoLanguages(owner, repo) {
   try {
     const response = await octokit.request(`GET /repos/${owner}/${repo}/languages`, {
       owner: owner,
@@ -39,5 +23,3 @@ async function getRepoLanguages(owner, repo) {
     throw error;
   }
 }
-
-export default router;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 import express from "express";
-import apiRoutes from "./api/index.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import path from "path";
+import { getRepoLanguages } from "./api/index.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const staticPath = path.join(__dirname, "static");
 
 const app = express();
+app.set('view engine', 'ejs');
 
-app.use("/api", apiRoutes);
-
-app.get("/", (req, res) => {
+const router = express.Router();
+router.get("/", (req, res) => {
   res.send("Hello World!");
-})
+});
 
+router.get("/api", async (req, res) => {
+  let username = req.query.username;
+  let repo = req.query.repo;
+
+  let langs = await getRepoLanguages(username, repo);
+  let totalUsage = Object.values(langs).reduce((a, b) => a + b, 0);
+  let percentUsage = Object.values(langs).map(usage => (usage / totalUsage) * 100);
+  Object.keys(langs).forEach((key, index) => {
+    langs[key] = percentUsage[index].toFixed(2);
+  })
+
+  res.render(path.join(staticPath, "index.ejs"), { langs });
+});
+
+app.use("/", router);
 app.listen(3000);
