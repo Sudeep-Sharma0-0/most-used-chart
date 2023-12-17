52380f2047c292a5b8d2835ffb82a7f4a3445a80
Sudeep Sharma <sharma.sudip1122@gmail.com>
2023-12-17 18:35:12 +0545

    Finished rendering piechart.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { fileURLToPath } from "url";
 import { dirname } from "path";
 import path from "path";
 import { getLangs } from "./getLangs.js";
+import { getColor } from "./getColor.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -16,14 +17,29 @@ app.use(express.static(staticPath));
 
 const router = express.Router();
 router.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.redirect("/api");
 });
 
 router.get("/api", async (req, res) => {
   let username = req.query.username;
 
   let langs = await getLangs(username);
-  res.render(path.join(viewPath, "index.ejs"), { langs });
+
+  let styles = "background: conic-gradient(";
+  let startPercentage = 0;
+  let endPercentage = 0;
+  let langKeys = Object.keys(langs);
+  langKeys.forEach((lang, index) => {
+    endPercentage += parseFloat(langs[lang]);
+    styles += `\n  ${getColor(lang, path.join(staticPath, "languages.yml"))} ${startPercentage}% ${endPercentage}%`;
+    startPercentage = endPercentage;
+    if (index !== langKeys.length - 1) {
+      styles += ",";
+    }
+  });
+  styles += "\n);";
+
+  res.render(path.join(viewPath, "index.ejs"), { langs, styles });
 });
 
 app.use("/", router);
